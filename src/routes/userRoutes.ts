36bfddfb41118c57047d0ec081701f5c493c7b72
Guilherme Faccin <gfaccin27@gmail.com>
2025-03-13 18:09:40 -0300

    feat: Separating dynamic and static routes

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -7,9 +7,11 @@ import {
 } from "../controllers/userController";
 
 export default async function userRoutes(app: FastifyInstance) {
+  // Rotas estáticas
   app.post("/register", registerUser);
-
+  app.get("/", getAllUsers);
   // app.get('/me', {preHandler: [app.authenticate]}, getUserData)
+  
+  // Rotas dinâmicas abaixo
   app.get("/:id", getUserById);
-  app.get("/", getAllUsers)
 }
--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import app from "./app";
 
 const start = async () => {
   try {
-    await app.listen({ port: 3050});
-    console.log("🚀 Server is running on http://localhost:3000");
+    await app.listen({ port: 3535});
+    console.log("🚀 Server is running on http://localhost:3535");
   } catch (err) {
     console.error("❌ Error starting server:", err);
     process.exit(1);
